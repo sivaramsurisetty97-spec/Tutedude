a5ec0389c8a491a408de9e20659f5f9eff608e4b
Sivaram <sivaram.surisetty97@gmail.com>
2026-01-06 23:03:26 +0530

    Committing Assignment 1

---
+++ Task1.py
@@ -0,0 +1,3 @@
+a= int(input("Enter first number ="))
+b= int(input("Enter Second number ="))
+print("\nAddition =", a+b,"\nSubtraction =",a-b,"\nMultiplication =", a*b,"\nDivision =", a/b)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+fir_name=input("Please enter your first name: ")
+las_name=input("Please enter your last name: ")
+
+print("Hello,",fir_name,las_name,"\b! Welcome to python programming.")
